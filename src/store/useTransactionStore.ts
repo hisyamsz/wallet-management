ae159cac3c33aed5356d4605a90b532856fbf292
hisyam <hisyam.s901@gmail.com>
2025-10-29 16:30:23 +0700

    feat: connect transaction store with balance store to auto-update balance on transactions

--- src/store/useTransactionStore.ts
+++ src/store/useTransactionStore.ts
@@ -2,6 +2,7 @@ import type { ITransaction } from "@/types/Transaction";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { v4 as uuid } from "uuid";
+import { useBalanceStore } from "./useBalanceStore";
 
 type TransactionStore = {
   transactions: ITransaction[];
@@ -10,20 +11,42 @@ type TransactionStore = {
   editTransaction: (t: ITransaction) => void;
 };
 
+const id = uuid();
+const { addBalance, subtractBalance } = useBalanceStore.getState();
+
 export const useTransactionStore = create<TransactionStore>()(
   persist(
     (set) => ({
       transactions: [],
 
-      addTransaction: (transaction) =>
+      addTransaction: (transaction) => {
+        if (transaction.type.toLowerCase() === "amount") {
+          addBalance(transaction.amount);
+        } else {
+          subtractBalance(transaction.amount);
+        }
+
         set((state) => ({
-          transactions: [...state.transactions, { id: uuid(), ...transaction }],
-        })),
+          transactions: [...state.transactions, { id, ...transaction }],
+        }));
+      },
 
       deleteTransaction: (id) =>
-        set((state) => ({
-          transactions: state.transactions.filter((tr) => tr.id !== id),
-        })),
+        set((state) => {
+          const transaction = state.transactions.find((tr) => tr.id === id);
+
+          if (transaction) {
+            if (transaction.type.toLowerCase() === "income") {
+              subtractBalance(transaction.amount);
+            } else {
+              addBalance(transaction.amount);
+            }
+          }
+
+          return {
+            transactions: state.transactions.filter((tr) => tr.id !== id),
+          };
+        }),
 
       editTransaction: (updatedTransaction) =>
         set((state) => ({
